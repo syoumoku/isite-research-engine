9a3fe58c849b00e699f605ca65e8b42077814635
ZHAOMU <469543889@qq.com>
2026-04-21 21:34:33 +0800

    Implement real Wikidata entity search adapter

--- data_sources/wikidata.py
+++ data_sources/wikidata.py
@@ -1,5 +1,21 @@
-"""Wikidata adapter placeholder."""
+from __future__ import annotations
+
+from .http_utils import http_get_json, polite_sleep
+
+
+WIKIDATA_SEARCH_URL = 'https://www.wikidata.org/w/api.php'
 
 
 def search_entity(name: str) -> dict:
-    return {"name": name, "entities": []}
+    data = http_get_json(
+        WIKIDATA_SEARCH_URL,
+        params={
+            'action': 'wbsearchentities',
+            'search': name,
+            'language': 'en',
+            'format': 'json',
+            'limit': 10,
+        },
+    )
+    polite_sleep(0.5)
+    return {'name': name, 'entities': data.get('search', []) if isinstance(data, dict) else []}
